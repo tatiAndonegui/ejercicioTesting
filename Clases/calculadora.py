009079d6cfe5332ef745b7e1dcdaa84424dc1317
tatiAndonegui <tatiana.andonegui@davinci.edu.ar>
2024-05-24 19:34:01 -0300

    creacion de la calculadora

---
+++ Clases/calculadora.py
@@ -0,0 +1,21 @@
+
+class Calculadora():
+
+    def suma(a,b):
+        suma = a+b
+        return suma
+
+    def resta(a,b):
+        resta = a-b
+        return resta
+
+
+    def multiplicacion(a,b):
+        multiplicacion = a*b
+        return multiplicacion
+
+
+    def division(a,b):
+        division =  a/b
+        return division
+
